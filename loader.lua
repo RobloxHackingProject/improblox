e395b4e64b6e28429358eafd36b488c2ac35c934
RobloxHackingProject <110852093+RobloxHackingProject@users.noreply.github.com>
2023-02-12 16:04:17 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,16 @@
+local places = {
+9534337535
+}
+for _, v in ipairs(places) do
+--Shrek in the Backrooms
+    if v == 9534337535 and 9534337535 == game.PlaceId then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/HPHub/main/NaturalDisasterSurvival.lua"))()
+    end
+
+
+--Universal Script
+if v == game.PlaceId then
+print('ImproBlox loaded succesfully')
+else
+print('ImproBlox universal script loaded succesfully')
+   end
